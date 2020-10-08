dd2bd5599d3c52e11354d420bfcb0710ff663449
Niraj Nandish <nirajnandish@gmail.com>
2020-10-08 21:55:25 +0400

    First two encryption algos

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+import base64
+
+abc = "abcdefghijklmnopqrstuvwxyz"
+cipher = sys.argv[1]
+
+after_e5 = base64.decodebytes(cipher.encode()).decode()
+
+after_e4 = "".join([abc[(abc.find(c)+13)%26] for c in after_e5])
+
+print(after_e4)
