a7eeb7eb0ee14d256b4e9d1996c52772e8de0616
Niraj Nandish <nirajnandish@gmail.com>
2020-10-08 22:56:06 +0400

    4th encryption algo

--- main.py
+++ main.py
@@ -10,4 +10,11 @@ after_e4 = "".join([abc[(abc.find(c)-13) % 26] for c in after_e5])
 
 after_e3 = "".join([abc[(abc.find(c)-20) % 26] for c in after_e4])
 
-print(after_e3)
+v_key = 'cryptography'
+for i in range(len(after_e3)-len(v_key)):
+    v_key += v_key[i % len(v_key)]
+
+after_e2 = "".join([abc[(abc.find(c)-abc.find(v)) % 26] for c, v in zip(after_e3, v_key)])
+
+print(after_e2)
+
