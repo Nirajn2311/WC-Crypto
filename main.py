93849b09cc003d926a005896990ae970ff78c7ce
Niraj Nandish <nirajnandish@gmail.com>
2020-10-08 22:40:48 +0400

    fix: Bug in 2nd and 3rd encryption algo

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ cipher = sys.argv[1]
 
 after_e5 = base64.decodebytes(cipher.encode()).decode()
 
-after_e4 = "".join([abc[(abc.find(c)+13) % 26] for c in after_e5])
+after_e4 = "".join([abc[(abc.find(c)-13) % 26] for c in after_e5])
 
-after_e3 = "".join([abc[(abc.find(c)+(20-1)) % 26] for c in after_e5])
+after_e3 = "".join([abc[(abc.find(c)-20) % 26] for c in after_e4])
 
 print(after_e3)
